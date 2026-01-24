adeb4e00a86e1e04969ceb3390ddbcc279fd27eb
Lua Logic <lualogicdev@gmail.com>
2026-01-24 00:26:41 -0500

    Properties update

--- [Scripts]/nolag_properties/fxmanifest.lua
+++ [Scripts]/nolag_properties/fxmanifest.lua
@@ -1,12 +1,15 @@
+shared_script "@ReaperV4/imports/bypass.lua"
+shared_script "@ReaperV4/imports/bypass_s.lua"
+shared_script "@ReaperV4/imports/bypass_c.lua"
+lua54 "yes" -- needed for Reaper
+
 fx_version 'cerulean'
 use_experimental_fxv2_oal 'yes'
 game 'gta5'
 lua54 'yes'
-
 description 'Properties system for FiveM.'
 author 'NoLag Development'
 version '2.3.95'
-
 shared_scripts {
   '@ox_lib/init.lua',
   'config.lua',
@@ -20,12 +23,10 @@ shared_scripts {
   'custom/clothes/*.lua',
   'custom/shells/*.lua',
 }
-
 ox_libs {
   'locale',
   'table',
 }
-
 client_scripts {
   'client/property/*.lua',
   'client/building/*.lua',
@@ -38,7 +39,6 @@ client_scripts {
   'custom/furniture/*.lua',
   'client/*.lua',
 }
-
 server_scripts {
   '@oxmysql/lib/MySQL.lua',
   'server/frameworks/*.lua',
@@ -46,11 +46,9 @@ server_scripts {
   'custom/banking/*.lua',
   'server/*.lua',
 }
-
 -- ui_page 'http://localhost:3000'
 ui_page 'web/build/index.html'
 nui_callback_strict_mode 'true'
-
 files {
   'web/build/index.html',
   'web/build/**/*',
@@ -58,10 +56,8 @@ files {
   'data/nolag_properties.dat54.rel',
 	'audiodirectory/properties_sounds.awc'
 }
-
 data_file 'AUDIO_WAVEPACK' 'audiodirectory'
 data_file 'AUDIO_SOUNDDATA' 'data/nolag_properties.dat'
-
 escrow_ignore {
   'client/frameworks/*.lua',
   'client/Editable.lua',
@@ -82,7 +78,6 @@ escrow_ignore {
   'furniture.lua',
   'config.lua'
 }
-
 dependencies {
   '/server:9549',
   '/onesync',
@@ -91,5 +86,4 @@ dependencies {
   'bob74_ipl',
   'screencapture'
 }
-
 dependency '/assetpacks'
