bc3ab5fbcad7922b6eb8412c423f1b736d94f2ba
Lua Logic <lualogicdev@gmail.com>
2026-02-10 10:44:50 -0500

    Possible seatbelt sound fix

--- [Scripts]/mst_seatbelt/fxmanifest.lua
+++ [Scripts]/mst_seatbelt/fxmanifest.lua
@@ -1,25 +1,32 @@
-shared_script "@ReaperV4/imports/bypass.lua"
-shared_script "@ReaperV4/imports/bypass_s.lua"
-shared_script "@ReaperV4/imports/bypass_c.lua"
-lua54 "yes" -- needed for Reaper
-
 fx_version 'cerulean'
 game 'gta5'
+
 lua54 'yes'
-version '1.0.6'
+
+version '1.0.7'
+
 author 'Mosoto Scripts'
 description 'The most advanced seatbelt system for FiveM'
+
+dependencies {
+    'interact-sound'
+}
+
 shared_scripts {
     'config.lua',
     '@ox_lib/init.lua',
 }
+
 client_scripts {
+    'code/modules/editable.lua',
     'code/modules/client.lua',
 }
+
 server_scripts {
     'code/modules/server.lua',
     'code/core/version_checker.lua'
 }
+
 files {
     'code/web/index.html',
     'code/web/styles.css',
@@ -27,9 +34,13 @@ files {
     'code/web/assets/indicator.png',
     'code/web/assets/heartbeat.ogg'
 }
+
 ui_page 'code/web/index.html'
+
 data_file 'DLC_ITYP_REQUEST' 'mst_seatbelt.ytyp'
+
 escrow_ignore {
     'config.lua',
+    'code/modules/editable.lua',
 }
 dependency '/assetpacks'
