b82538437383c37001b89b8bc827157ddbd58a8f
Lua Logic <lualogicdev@gmail.com>
2026-02-24 18:48:35 -0500

    Prep updates

--- [Scripts]/[Update]/lorp_seizure/sql/update_1.0.5_esx_qb-core.sql
+++
@@ -1,4 +0,0 @@
--- ℹ️ Run this SQL when you update the script. For the initial installation, just insert the normal version!
-
-INSERT INTO police_impound_offence (`id`, `name`, `amount`) VALUES
-('auto_impound', 'Auto Impound', 50);
