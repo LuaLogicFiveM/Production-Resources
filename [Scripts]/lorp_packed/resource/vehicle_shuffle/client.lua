10953e6218995b6fb6f48d9bad27a24fdeb85920
Lua Logic <lualogicdev@gmail.com>
2026-02-24 18:44:31 -0500

    Fixed vehicle seat shuffle

--- [Scripts]/lorp_packed/resource/vehicle_shuffle/client.lua
+++ [Scripts]/lorp_packed/resource/vehicle_shuffle/client.lua
@@ -1,4 +1,4 @@
-local r_handcuffs = exports.r_handcuffs
+--local r_handcuffs = exports.r_handcuffs
 local mst_seatbelt = exports['mst_seatbelt']
 
 local function disableAutoShuffle(seatIndex)
@@ -12,9 +12,9 @@ end
 lib.onCache('seat', disableAutoShuffle)
 
 local function shuffleSeat(self)
-    if r_handcuffs:IsPlayerCuffed(cache.serverId) then
+    --[[if r_handcuffs:IsPlayerCuffed(cache.serverId) then
         return lib.notify({title = 'Vehicle', description = 'You are unable to do this while handcuffed', type = 'error', position = 'top'})
-    end
+    end]]
 
     if mst_seatbelt:HasSeatbelt() then
         return lib.notify({title = 'Vehicle', description = 'You are unable to do this with your seatbelt enabled', type = 'error', position = 'top'})
