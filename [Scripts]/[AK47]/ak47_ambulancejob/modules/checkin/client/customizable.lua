17ad09dfc25e55c7b4869b3f62a666c8be5258d0
Lua Logic <lualogicdev@gmail.com>
2026-02-24 18:52:28 -0500

    More prep updates

--- [Scripts]/[AK47]/ak47_ambulancejob/modules/checkin/client/customizable.lua
+++ [Scripts]/[AK47]/ak47_ambulancejob/modules/checkin/client/customizable.lua
@@ -2,5 +2,6 @@ AddEventHandler('ak47_ambulancejob:onFinishCheckin', function()
     -- your code here
     if Config.SetCrutchAfterCheckin then
         TriggerServerEvent('ak47_crutch:set', GetPlayerServerId(PlayerId()), Config.CrutchTimer) -- 5 minutes
+        --exports.lorp_packed:ForceCrutch(Config.CrutchTimer)
     end
 end)
