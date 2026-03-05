e13b387ca881a97a7d721a6673ef635506504bf0
Lua Logic <lualogicdev@gmail.com>
2026-03-05 17:23:12 -0500

    chop shop payouts updated

--- [Scripts]/lorp_packed/resource/chop_shop/server.lua
+++ [Scripts]/lorp_packed/resource/chop_shop/server.lua
@@ -4,7 +4,7 @@ RegisterNetEvent('lorp_chopshop:server:requestChoppedVehicle')
 AddEventHandler('lorp_chopshop:server:requestChoppedVehicle', function()
     if GetInvokingResource() then return end
     local src = source
-    local count = math.random(1500, 3500)
+    local count = math.random(150, 350)
 
     sv_utils.addItem(src, 'black_money', count)
     exports['lorp_packed']:SendLog('Chop Shop', GetPlayerName(src).. ' has used the chop shop and made '..count..' dirty money.', 'https://discord.com/api/webhooks/1231453425246670939/BZFl46RzLIpbe-Ec6Tq3W2NExaLIqy_l05_x3-bH6F_fq07f08H60qTt9juVvs4x9g-E')
