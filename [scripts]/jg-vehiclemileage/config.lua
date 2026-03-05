0048415fcd45ec35fa458f7c126d5e06c4dff998
Lua Logic <lualogicdev@gmail.com>
2026-03-05 17:21:25 -0500

    Added vehicle mileage

---
+++ [scripts]/jg-vehiclemileage/config.lua
@@ -0,0 +1,7 @@
+Config = {}
+Config.AutoRunSQL = true
+
+Config.Framework = "auto" -- or "QBCore", "Qbox", "ESX"
+Config.ShowMileage = true
+Config.Unit = "miles" -- "miles" or "kilometers"
+Config.Position = "bottom-center" -- "bottom-right" or "bottom-left" or "top-right" or "top-left" or "bottom-center" or "top-center"
