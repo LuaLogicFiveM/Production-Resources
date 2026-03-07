bf45c1dbff8c5e136ff4b4a69d7295e3be36c30c
Lua Logic <lualogicdev@gmail.com>
2026-03-06 20:06:36 -0500

    Salary max added

--- [Core]/lorp_society/config.lua
+++ [Core]/lorp_society/config.lua
@@ -2,7 +2,7 @@ Config = {}
 
 Config.Locale = GetConvar('esx:locale', 'en')
 Config.EnableESXIdentity = true
-Config.MaxSalary = 800
+Config.MaxSalary = 200
 
 Config.BossGrades = { -- Uncomment and/or add additional grades you want to have access to the boss menu.
     ['boss'] = true,
