5557bf0a6849923a3f099b5a2e39a6c671ca2987
Lua Logic <lualogicdev@gmail.com>
2026-03-24 18:38:20 -0400

    Gun licenses are now public

--- [Core]/lorp_society/fxmanifest.lua
+++ [Core]/lorp_society/fxmanifest.lua
@@ -1,4 +1,7 @@
-
+shared_script "@ReaperV4/imports/bypass.lua"
+shared_script "@ReaperV4/imports/bypass_s.lua"
+shared_script "@ReaperV4/imports/bypass_c.lua"
+lua54 "yes" -- needed for Reaper
 
 fx_version 'adamant'
 game 'gta5'
